d39ee0356b3f3e3a4230341fd69c7de9157be16d
Artur Penar <penar.art@gmail.com>
2025-05-15 22:04:21 +0200

    Initail commit

---
+++ weather_receiver.py
@@ -0,0 +1,52 @@
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 49.6887,
+    "longitude": 21.7706,
+    "hourly": ["temperature_2m", "cloud_cover", "global_tilted_irradiance"],
+    "forecast_days": 3
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
+hourly_global_tilted_irradiance = hourly.Variables(2).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+    freq = pd.Timedelta(seconds = hourly.Interval()),
+    inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["global_tilted_irradiance"] = hourly_global_tilted_irradiance
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+# Save the dataframe to a CSV file
+output_file = "hourly_weather_data.csv"
+hourly_dataframe.to_csv(output_file, index=False)
+print(f"Data saved to {output_file}")
