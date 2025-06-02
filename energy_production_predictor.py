2a1036b4b52d17f219fb42ee145fc88891c4d459
Artur Penar <penar.art@gmail.com>
2025-06-02 14:45:53 +0200

    Fix output pivot path in EnergyProductionPredictor initialization

--- energy_production_predictor.py
+++ energy_production_predictor.py
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     predictor = EnergyProductionPredictor(
         input_path="data/input/production_to_predict.xlsx",
         output_pred_path="data/input/pv_predicted.xlsx",
-        output_pivot_path="data/output/pv_pivot2.xlsx"
+        output_pivot_path="data/output/pv_pivot.xlsx"
     )
     predictor.run()
